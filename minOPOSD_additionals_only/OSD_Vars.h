6f71c7f1f29a92b8c197a1ac706bd3fa9884b62a
TeamRedFox001@gmail.com <TeamRedFox001@gmail.com@76497834-64f1-9877-dd3b-0bdaccbdb7bd>
2013-02-21 10:07:04 +0000

    cosmetics
    git-svn-id: http://minoposd.googlecode.com/svn/trunk@189 76497834-64f1-9877-dd3b-0bdaccbdb7bd

--- minOPOSD_additionals_only/OSD_Vars.h
+++ minOPOSD_additionals_only/OSD_Vars.h
@@ -3,20 +3,12 @@
 //
 
 
-// JRChange: OpenPilot UAVTalk:
-static uint8_t  op_alarm = 0;			// OP alarm info
-static uint8_t  osd_armed = 0;			// OP armed info
-static uint8_t  osd_time_hour = 0;		// OP GPS time hour info
-static uint8_t  osd_time_minute = 0;		// OP GPS tiem minute info
-
-// JRChange: Flight Batt on MinimOSD:
-static int	volt_div_ratio = 0;		// Volt * 100
-static int	curr_amp_per_volt = 0;		// Ampere * 100
-static int	curr_amp_offset = 0;		// Ampere * 10000
-static uint16_t osd_total_A = 0;                // Battery total current [mAh]
+#define npanels		2			    // # of possible panels
 
 
-// Panel variables
+static boolean      waitingTelemetry = 1;
+
+static uint8_t      panel = 0;                      // active panel: 0 = first panel. 1 = second panel, 2 = panel off
 
 static bool         motor_armed = 0;
 
@@ -37,37 +29,40 @@ static uint8_t      batt_warn_level = 0;
 static float        start_Time = -1.0;
 static uint8_t      osd_mode = 0;                   // FlightMode of the FlightControl
 
-static float        osd_climb = 0;                  // climb rate
-
-static float        osd_lat = 0;                    // latidude
-static float        osd_lon = 0;                    // longitude
 static uint8_t      osd_satellites_visible = 0;     // number of satelites
 static uint8_t      osd_fix_type = 0;               // GPS lock 0-1=no fix, 2=2D, 3=3D
-
-static uint8_t      osd_got_home = 0;               // tells if got home position or not
-static float        osd_home_lat = 0;               // home latidude
-static float        osd_home_lon = 0;               // home longitude
-static float        osd_home_alt = 0;
-static long         osd_home_distance = 0;          // distance from home
-static uint8_t      osd_home_direction;             // Arrow direction pointing to home (1-16 to CW loop)
-
-static int16_t      osd_pitch = 0;                  // pitch from DCM
-static int16_t      osd_roll = 0;                   // roll from DCM
-static int16_t      osd_yaw = 0;                    // relative heading form DCM
-static float        osd_heading = 0;                // ground course heading from GPS
-
+static float        osd_lat = 0;                    // latidude
+static float        osd_lon = 0;                    // longitude
 static float        osd_alt = 0;                    // altitude
+static float        osd_climb = 0;                  // climb rate
+static float        osd_groundspeed = 0;            // ground speed
+static float        osd_heading = 0;                // ground course heading
+
+static int16_t      osd_roll = 0;                   // roll from FC
+static int16_t      osd_pitch = 0;                  // pitch from FC
+static int16_t      osd_yaw = 0;                    // relative heading form FC
+static uint16_t     osd_throttle = 0;               // throttle
 
 static uint8_t      osd_alt_cnt = 0;                // counter for stable osd_alt
 static float        osd_alt_prev = 0;               // previous altitude
+static uint8_t      osd_got_home = 0;               // tells if got home position or not
+static float        osd_home_lat = 0;               // home latidude
+static float        osd_home_lon = 0;               // home longitude
+static float        osd_home_alt = 0;               // home altitude
+static long         osd_home_distance = 0;          // distance from home
+static uint8_t      osd_home_direction;             // Arrow direction pointing to home (1-16 to CW loop)
 
-static float        osd_groundspeed = 0;            // ground speed
-static uint16_t     osd_throttle = 0;               // throtle
+// OpenPilot UAVTalk:
+static uint8_t      op_alarm = 0;                   // OP alarm info
+static uint8_t      osd_armed = 0;                  // OP armed info
+static uint8_t      osd_time_hour = 0;              // OP GPS time hour info
+static uint8_t      osd_time_minute = 0;            // OP GPS tiem minute info
 
-static boolean      waitingTelemetry = 1;
-
-static const uint8_t npanels = 2;
-static uint8_t panel = 0;
+// Flight Batt on MinimOSD:
+static int          volt_div_ratio = 0;             // Volt * 100
+static int          curr_amp_per_volt = 0;          // Ampere * 100
+static int          curr_amp_offset = 0;            // Ampere * 10000
+static uint16_t     osd_total_A = 0;                // Battery total current [mAh]
 
 
 // Panel BIT registers
